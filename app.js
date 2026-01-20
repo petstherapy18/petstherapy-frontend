2af2bf1515cc4d37f2748af7a3ab1740a5507016
Yineth <petstherapy18@gmail.com>
2026-01-19 21:48:56 -0500

    Actualizar URL backend producción

--- app.js
+++ app.js
@@ -608,21 +608,32 @@ const edad = document.getElementById("npEdad").value.trim();
   // 📡 ENVÍO A BACKEND
   // ----------------------------
   try {
-    const res = await fetch(
-      "https://petstherapy-backend.onrender.com/api/pacientes/nuevo",
-      {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-          nombre,
-          especie,
-          raza,
-          edad,
-          propietarioCorreo,
-          foto: ""
-        })
-      }
-    );
+    const token = sessionStorage.getItem("token");
+
+if (!token) {
+  mostrarBurbuja("Sesión expirada, inicia sesión nuevamente", "error");
+  return;
+}
+
+const res = await fetch(
+  "https://petstherapy-backend.onrender.com/api/pacientes/nuevo",
+  {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${token}`
+    },
+    body: JSON.stringify({
+      nombre,
+      especie,
+      raza,
+      edad,
+      propietarioCorreo,
+      foto: ""
+    })
+  }
+);
+
 
     const data = await res.json();
     if (!res.ok) throw new Error(data.message || "Error al crear paciente");
